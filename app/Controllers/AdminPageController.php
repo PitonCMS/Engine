652897fe85c87fa0fca3f118998139eea3eb1cf5
Wolfgang Moritz <wolfmoritz@gmail.com>
2018-12-09 13:32:07 +0000

    Fixed issue in add new element form to honor elementTypeOptions list.

--- app/Controllers/AdminPageController.php
+++ app/Controllers/AdminPageController.php
@@ -140,7 +140,12 @@ class AdminPageController extends BaseController
     /**
      * Fetch New Element Form
      *
-     * Responds to ajax requests for element form
+     * Renders new element form with initial values, and returns via Ajax to browser.
+     * At a minimum, the element form is expecting these values:
+     * - sectionCodeName
+     * - elementType
+     * - elementSort
+     * - elementTypeOptions | optional, comma separated list of approved element types
      */
     public function fetchElementForm($request, $response, $args)
     {
@@ -149,6 +154,11 @@ class AdminPageController extends BaseController
         $form['sectionCodeName'] = $parsedBody['sectionCodeName'];
         $form['elementType'] = $parsedBody['elementType'];
         $form['elementSort'] = 1;
+
+        // Only include element type options if the string is not empty
+        if (!empty($parsedBody['elementTypeOptions'])) {
+            $form['elementTypeOptions'] = explode(',', $parsedBody['elementTypeOptions']);
+        }
 
         $elementFormHtml = $this->container->view->fetch('@admin/editElementFormLoad.html', ['data' => $form]);
 
