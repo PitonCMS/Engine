2f5aa93cf8870ddd177bd42b227c6c6558e77a55
Wolfgang Moritz <wolfmoritz@gmail.com>
2020-06-27 12:31:45 +0100

    Updated formControl.js to support button type reset.

--- assets/js/modules/formControl.js
+++ assets/js/modules/formControl.js
@@ -1,23 +1,46 @@
-/**
- * Enable Form Control
- *
- * @param {object} control Control button element
- */
-const enableFormControl = function(control) {
-    if (control && control.disabled) {
-        control.disabled = false;
-    }
-}
+// --------------------------------------------------------
+// Form Controls. Enables controls on form input, and prompts on reset and delete
+// --------------------------------------------------------
 
-/**
- * Disable Form Control
- *
- * @param {object} control Control button element
- */
-const disableFormControl =  function(control) {
-    if (control && !control.disabled) {
-        control.disabled = true;
-    }
-}
+// Form Control Events
+document.querySelectorAll("form").forEach(form => {
+    // Disable form controls and listen for form input changes to re-enable save controls
+    let controls = form.querySelectorAll(`[data-form-button="save"], [data-form-button="cancel"]`);
 
-export { enableFormControl, disableFormControl };
+    if (controls) {
+        controls.forEach(control => {
+            control.disabled = true;
+        });
+
+        // Listen for form changes to reenable controls
+        form.addEventListener("input", (e) => {
+            controls.forEach(control => {
+                control.disabled = false;
+            });
+        }, false);
+    }
+
+    // Confirm discard of changes
+    form.querySelectorAll(`[data-form-button="cancel"]`).forEach(control => {
+        control.addEventListener("click", (event) => {
+            if(!confirm("Click Ok to discard your changes, or Cancel continue editing.")) {
+                event.preventDefault();
+                return;
+            }
+
+            // Reload page if a url was provided
+            if (event.target.dataset.formResetHref) {
+                event.preventDefault();
+                window.location = event.target.dataset.formResetHref;
+            }
+            // Otherwise let type="reset" reset form as default event
+        }, false);
+    });
+
+    // Confirm delete
+    form.querySelectorAll(`[data-delete-prompt]`).forEach(control => {
+        control.addEventListener("click", (e) => {
+            if (!confirm(e.target.dataset.deletePrompt)) e.preventDefault();
+        }, false);
+    });
+});
--- assets/js/modules/main.js
+++ assets/js/modules/main.js
@@ -2,42 +2,9 @@
 // Main JS
 // --------------------------------------------------------
 
-import { enableFormControl, disableFormControl } from './formControl.js';
+import './formControl.js';
 import { dismissAlertInlineMessage } from './alert.js';
 import { collapseToggle } from './collapse.js';
-
-// Form Control Events
-document.querySelectorAll("form").forEach(form => {
-    // Disable form controls and listen for form input changes to re-enable save controls
-    let saveButtons = form.querySelectorAll(`[data-form-button="save"]`);
-    if (saveButtons) {
-        saveButtons.forEach(control => {
-            disableFormControl(control);
-        });
-
-        // Listen for form changes to reenable controls
-        form.addEventListener("input", (e) => {
-            saveButtons.forEach(control => {
-                enableFormControl(control);
-            });
-        }, false);
-    }
-
-    // Confirm discard of changes
-    form.querySelectorAll(`[data-form-button="cancel"]`).forEach(control => {
-        control.addEventListener("click", (e) => {
-            let userResponse = confirm("Click Ok to discard your changes, or cancel continue editing?");
-            if (!userResponse) e.preventDefault();
-        }, false);
-    });
-
-    // Confirm delete
-    form.querySelectorAll(`[data-delete-prompt]`).forEach(control => {
-        control.addEventListener("click", (e) => {
-            if (!confirm(e.target.dataset.deletePrompt)) e.preventDefault();
-        }, false);
-    });
-});
 
 // Binding click events to document
 document.addEventListener("click", dismissAlertInlineMessage, false);
