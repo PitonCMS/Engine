002c21b2b100556a928036c4221481b5f1b069f2
Wolfgang Moritz <wolfmoritz@gmail.com>
2020-07-19 09:20:40 +0100

    Updated collapse.js to include auto collapse data-collapse-auto attribute option.

--- assets/js/modules/collapse.js
+++ assets/js/modules/collapse.js
@@ -1,3 +1,6 @@
+// Class name to toggle to show/hide elements
+const collapseClass = "collapsed";
+
 /**
  * Collapse Toggle
  * @param {Event} event
@@ -7,7 +10,20 @@ const collapseToggle = function(event) {
     let toggleKey = event.target.closest(`[data-collapse-toggle]`).dataset.collapseToggle;
 
     // Find the matching collapse target by value and toggle class
-    document.querySelector(`[data-collapse-target="${toggleKey}"]`)?.classList.toggle("collapsed");
+    let collapseTarget = document.querySelector(`[data-collapse-target="${toggleKey}"]`);
+    collapseTarget.classList.toggle(collapseClass);
+}
+
+/**
+ * Auto Collapse
+ * @param {Event} event
+ */
+const autoCollapse = function(event) {
+    if (!event.target.closest(`[data-collapse-auto]`)) return;
+
+    let collapseTarget = event.target.closest(`[data-collapse-auto]`);
+    collapseTarget.classList.toggle(collapseClass);
 }
 
 document.addEventListener("click", collapseToggle, false);
+document.addEventListener("click", autoCollapse, false);
