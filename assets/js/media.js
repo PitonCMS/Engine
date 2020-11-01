b9c1739ff617421201d6ca9c5a17cfc83fc910ad
Wolfgang Moritz <wolfmoritz@gmail.com>
2020-11-01 10:22:25 +0000

    Enabled click to copy to clipboard icon on media images with JS.

--- assets/js/media.js
+++ assets/js/media.js
@@ -35,6 +35,10 @@ const saveMedia = function(event) {
         });
 }
 
+/**
+ * Delete Media Asynchronously
+ * @param {Event} event
+ */
 const deleteMedia = function(event) {
     if (!event.target.dataset.deleteMediaPrompt) return;
     if (!confirm(event.target.dataset.deleteMediaPrompt)) return;
@@ -56,5 +60,28 @@ const deleteMedia = function(event) {
         });
 }
 
+/**
+ * Copy Media Path on Click
+ *
+ * Copies relative path to media file
+ * @param {Event} event
+ */
+const copyMediaPath = function(event) {
+    if (!event.target.dataset.mediaClickCopy) return;
+
+    try {
+        // Stop if the current browser does not support navigator clipboard
+        if (!navigator.clipboard) {
+            throw "Your browser does not support click to copy.";
+        }
+
+        let dataPath = event.target.dataset.mediaClickCopy;
+        navigator.clipboard.writeText(dataPath);
+    } catch (error) {
+        alert("Error in click to copy: " + error);
+    }
+}
+
 document.addEventListener("click", saveMedia, false);
 document.addEventListener("click", deleteMedia, false);
+document.addEventListener("click", copyMediaPath, false);
